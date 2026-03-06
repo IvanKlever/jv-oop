3aa11484cef9aa4b4e03a0236902e2a96d3cb76e
Ivan Lydin <ivanlydin2008@gmail.com>
2026-03-06 22:27:54 +0200

    task completed

---
+++ src/main/java/core/basesyntax/Bulldozer.java
@@ -0,0 +1,15 @@
+package core.basesyntax;
+
+public class Bulldozer extends Machine {
+    public Bulldozer (){
+
+    }
+
+    public void doWork() {
+        System.out.println("Bulldoer started its work!");
+    }
+
+    public void stopWork() {
+        System.out.println("Billdozer stopped working!");
+    }
+}
